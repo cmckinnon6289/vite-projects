2626b3e236c69a68e38b2ad64f99d62127b840d0
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-01-16 11:29:28 -0500

    dynamic router

---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,21 @@
+<script>
+import { ref, onMounted } from 'vue'
+
+const event = ref(null)
+
+onMounted(() => {
+
+})
+</script>
+
+<template>
+    <div>
+        <h1>{{ event.title }}</h1>
+        <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+        <p>{{ event.description }}</p>
+    </div>
+</template>
+
+<style>
+
+</style>
