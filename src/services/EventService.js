3e06e3d531e4f95cfea5ed23f47f9e91c1a40c70
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2024-01-18 10:27:17 -0500

    done ??

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,19 +1,20 @@
-import axios from "axios"
-
+import axios from 'axios'
+ 
 const apiClient = axios.create({
-    baseURL: "https://my-json-server.typicode.com/cmckinnon6289/vite-projects",
-    withCredentials: false,
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    }
+  baseURL: 'https://my-json-server.typicode.com/cmckinnon6289/vite-projects',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
 })
-
+ 
 export default {
-    getEvents() {
-        return apiClient.get('events')
-    },
-    getEvent(id) {
-        return apiClient.get('/events/',id)
-    }
+  getEvents() {
+    return apiClient.get('/events')
+  },
+  //Added new call
+  getEvent(id) {
+    return apiClient.get(`/events/${id}`)
+  }
 }
